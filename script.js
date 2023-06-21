3171706b025501a3e52ed46d98cfb8de9b094fab
murilo46 <murilosavianp@gmail.com>
2023-06-21 13:31:02 -0300

    atualizei

---
+++ script.js
@@ -0,0 +1,10 @@
+// Exemplo: Alterar o texto do cabeçalho
+const header = document.querySelector('header h1');
+header.textContent = 'Área de Procedimento ao Cliente';
+
+// Exemplo: Adicionar um novo estilo CSS
+const redondoDiv = document.querySelector('.redondo');
+redondoDiv.style.border = '2px solid #333';
+redondoDiv.style.boxShadow = '0 0 5px #333';
+
+document.getElementsByTagName("h1")[0].style.fontSize = "6vw";
